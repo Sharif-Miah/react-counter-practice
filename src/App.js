2d0b3702c3d6757bd8d6b6f4755aa14d176c6813
farhan sharif <farhansharif1410@gmail.com>
2022-09-20 21:17:23 +0530

    user api added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <UsersApi />
     </div>
   );
 }
@@ -30,6 +31,34 @@ function Counter() {
   )
 }
 
+
+function UsersApi() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
+
+  return (
+    <div>
+      <h1>Users External</h1>
+      {
+        users.map(user => <Users name={user.name} email={user.email} />)
+      }
+    </div>
+  )
+}
+
+function Users(props) {
+  return (
+    <div>
+      <h1>Name: {props.name}</h1>
+      <h4>Email: {props.email}</h4>
+    </div>
+  )
+}
 
 
 export default App;
